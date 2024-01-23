fb18d41cfd68bd3489bb4a6deba27780af14f0ef
hfzfdlh <fadillahhafiz.fadillah@gmail.com>
2024-01-23 09:47:05 +0700

    add style and public directory

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const router = require('./routes')
 const app = express()
 const port = 3000
 
-
+app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
